8d3b425a405e1c6dcc44f18f5052a1d4a5023ea8
Pozharitsky <s.pozharitsky@intelico.su>
2021-05-28 11:40:21 +0300

    feat: add ButtonsGroup

---
+++ src/components/ButtonsGroup/ButtonsGroup.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import classNames from "classnames";
+import "./style.css";
+
+const ButtonsGroup = ({ children }) => {
+const [activeButtonIndex, setActiveButtonIndex] = useState(1);
+  return (
+    <div className="ButtonsGroup">
+      {React.Children.map(children, (child, i) =>
+        React.cloneElement(child, {
+          className: classNames("ButtonsGroup__item", {
+            "ButtonsGroup__item--active": activeButtonIndex === i,
+          }),
+          onClick: () => {
+            setActiveButtonIndex(i);
+          }
+        })
+      )}
+    </div>
+  );
+};
+
+ButtonsGroup.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]).isRequired,
+};
+
+export default ButtonsGroup;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Button from "./components/Button/Button";
+import ButtonsGroup from "./components/ButtonsGroup/ButtonsGroup";
 
 const App = () => {
   return (
     <div>
-      <Button>По умолчанию</Button>
+      {/* <Button>По умолчанию</Button>
       <Button mode="secondary" size='small'>Второстепенная</Button>
       <Button mode="success" size='large'>Успех</Button>
-      <Button mode="danger" disabled className="evilClass">Лажа</Button>
+      <Button mode="danger" disabled className="evilClass">Лажа</Button> */}
+      {/* <Button counter={5}>Счётчик</Button> */}
+
+      <ButtonsGroup>
+        <Button>Left part</Button>
+        <Button>Middle part</Button>
+        <Button>Right part</Button>
+      </ButtonsGroup>
     </div>
   );
 };
